482c9defc884b8474aa2c1eb84b1306f3ae2bbd3
walton <bigw369@gmail.com>
2017-09-09 14:39:43 -0400

    makes yarn build-able

--- src/App.js
+++ src/App.js
@@ -1,8 +1,5 @@
 import React, { Component } from 'react';
 import './App.css';
-var Web3 = require('web3')
-
-window.web3 = new Web3(window.web3.currentProvider);
 
 class App extends Component {
   state = {
@@ -10,26 +7,34 @@ class App extends Component {
     balance: undefined
   }
   componentDidMount() {
-    if (window.web3) {
-      let wallet;
-      window.web3.eth.getAccounts((err, accounts) => {
-        wallet = accounts[0];
-        window.web3.eth.getBalance(wallet).then((balance) => {
-          this.setState({ wallet, balance });
+    window.addEventListener("load", () => {
+      if (window.web3) {
+        let wallet;
+        window.web3.eth.getAccounts((err, accounts) => {
+          wallet = accounts[0];
+          window.web3.eth.getBalance(wallet, (err, balance) => {
+            this.setState({ wallet, balance });
+          });
         });
-      });
-    }
+      }
+    });
   }
 
   render() {
+    let body;
+    if (this.state.wallet) {
+      body = <div>your account is: {this.state.wallet} <br /> your balance is: {this.state.balance / Math.pow(10, 18)} ETH</div>;
+    } else {
+      body = "Loading...";
+    }
+
     return (
       <div className="App">
         <div className="App-header">
           <h2>Ethereum account viewer</h2>
         </div>
         <p className="App-intro">
-         your account is: {this.state.wallet} <br />
-         your balance is: {this.state.balance / Math.pow(10, 18)} ETH
+          {body}
         </p>
       </div>
     );
